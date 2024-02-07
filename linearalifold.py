82cb7f52fec9927955c1a4383aaf9fd60555b655
Apoorv Malik <apoorvmalik@10-249-87-48.wireless.oregonstate.edu>
2024-02-07 00:19:28 -0800

    update file

--- linearalifold.py
+++ linearalifold.py
@@ -6,68 +6,64 @@ import sys
 import argparse
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Command-line arguments parser")
+    parser = argparse.ArgumentParser(description="LinearAlifold: Linear-Time Consensus Structure Prediction for RNA Alignments")
 
-    parser.add_argument("-b", "--beam-size", type=int, default=100, help="set beam size, (DEFAULT=100)")
-    parser.add_argument("-vb", "--verbose", action="store_true", default=False, help="print out runtime in seconds, (DEFAULT=FALSE)")
-
+    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Enable verbose mode to print out detailed information (DEFAULT=FALSE)")
+    parser.add_argument("-b", "--beam", type=int, default=100, help="Set beam size (beam=0 will result in exact search) (DEFAULT=100)")
     parser.add_argument(
-        "-em", "--energy-model", type=int, choices=[1, 2], default=2, help="energy model choice, 1 (Vienna) or 2 (BL*), (DEFAULT=2)"
+        "-e", "--energy-model", type=int, choices=[1, 2], default=2, help="Energy model choice: 1 (Vienna) or 2 (BL*) (DEFAULT=2)"
     )
-    parser.add_argument("--multi-approx", type=int, default=0, help="use multiloop approximation, (DEFAULT=0 (False))")
-    parser.add_argument("-ct", "--cutoff", type=int, default=-40, help="cutoff threshold (DEFAULT=-40)")
-    parser.add_argument("-bt", "--beta", type = float, default=1.2, help="beta value (DEFAULT=1.2)")
-    parser.add_argument("-dt", "--delta", type = float, default=0.1, help="delta value (DEFAULT=0.1)")
+    parser.add_argument('-p', '--partition', action='store_true', help="Partition Mode: computes BPPs, MEA Structure, and Threshknot structure")
+    parser.add_argument('-s', '--sample', type=int, default=0, help="Sampling Mode: sample size for running sampling mode (0 means sampling mode is off) (DEFAULT=0)")
 
-    parser.add_argument("-bf", '--bpp-file', type=str, help='bpp file', default="")
-    parser.add_argument("-mf", '--mea-file', type=str, help='mea file', default="")
-    parser.add_argument("-tf", '--threshknot-file', type=str, help='threshknot file', default="")
-    parser.add_argument('-tt', '--threshknot-threshold', type=float, default=0.3, help='threshknot threshold')
+    parser.add_argument("-c", "--cutoff", type=int, default=-40, help="Set pairability/conservation score cutoff threshold (DEFAULT=-40)")
+    parser.add_argument("-y", "--beta", type = float, default=1.2, help="Set parameter beta value (DEFAULT=1.2)")
+    parser.add_argument("-z", "--delta", type = float, default=0.1, help="Set parameter delta value (DEFAULT=0.1)")
+    parser.add_argument('-t', '--threshknot-threshold', type=float, default=0.3, help='Set threshknot threshold')
+    
+    parser.add_argument('-B', "--bpp-file", type=str, help="BPP matrix save path", default="")
+    parser.add_argument('-M', "--mea-file", type=str, help="MEA structure save path", default="")
+    parser.add_argument('-T', "--threshknot-file", type=str, help="Threshknot structure save path", default="")
 
-    parser.add_argument('-pt', '--partition', action='store_true', help='run partition function')
-    parser.add_argument('-lz', '--lazy', action='store_true', help='run lazy outside', default=True)
+
+    parser.add_argument('-m', "--multi-approx", action="store_true", help="Use multiloop approximation (DEFAULT=FALSE)")
+    parser.add_argument('-o', "--non-lazy", action="store_false", help="Do not use lazy outside (DEFAULT=TRUE)")
     return parser.parse_args()
 
 def main():
     args = get_args()
-    is_verbose = "1" if args.verbose else "0"
-    path = os.path.dirname(os.path.abspath(__file__))
-    # print(args)
 
-    options = [str(args.beam_size),
-            is_verbose,
+    options = [str(args.beam),
+            "1" if args.verbose else "0",
             str(args.energy_model),
-            str(args.multi_approx),
+            "1" if args.multi_approx else "0",
+            "1" if args.partition else "0",
+
             str(args.cutoff),
             str(args.beta),
             str(args.delta),
-            "1" if args.partition else "0",
-            "1" if args.lazy else "0",
+            
+            "1" if args.non_lazy else "0",
             str(args.threshknot_threshold),
+            str(args.sample),
+
             args.bpp_file,
             args.mea_file,
             args.threshknot_file,
         ]
-
-    if args.energy_model == 1:
-        cmd = [
-            "%s/%s" % (os.path.dirname(os.path.abspath(__file__)), "bin/laf_mfe_vienna"),
-        ] + options
-
-    elif args.energy_model == 2:
-        cmd = [
-            "%s/%s" % (os.path.dirname(os.path.abspath(__file__)), "bin/laf_mfe_bl"),
-        ] + options
-
-    # Start the subprocess
+    
+    cmd = ["%s/%s" % (os.path.dirname(os.path.abspath(__file__)), \
+                      "bin/laf_mfe_vienna" if args.energy_model == 1 else "bin/laf_mfe_bl")] + options
+    
+    # start the subprocess
     process = subprocess.Popen(cmd, stdin=sys.stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Capture and print the output after the process completes
+    # capture and print the output after the process completes
     stdout, stderr = process.communicate()
     if stdout:
         print(stdout.decode())
     if stderr:
-        print("There was an error!")
+        print("[ERROR] See below for error message")
         print(stderr.decode())
 
 if __name__ == "__main__":
